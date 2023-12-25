ea13414d063e54f8370de1febfde61d70a4b96a9
Rogpe <paulo.rogpe@gmail.com>
2023-12-25 15:09:01 -0300

    logic

---
+++ OperadoresLogic.py
@@ -0,0 +1,24 @@
+# idade = 25
+# altura = 1.75
+
+# resultado = (idade >= 18) and (altura >= 1.70)
+# msg = 'Pode participar? ' + str(resultado)
+
+# print(msg)
+
+# Programa de alarme
+
+# porta = 'f'
+# janela = 'f'
+
+# alarme = (porta == 'f') or (janela == 'a')
+# msg = 'Porta ou janela aberta: ' + str(alarme)
+# print(msg)
+
+# Operador NOT
+
+tem_dinheiro = False
+tem_dinheiro = not tem_dinheiro
+
+msg = 'Tem dinheiro? ' + str(tem_dinheiro)
+print(msg)
