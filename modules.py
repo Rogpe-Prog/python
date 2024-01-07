f7af8d6e2df3b047ca9ed06f3d5628bfec1aedc6
Rogpe <paulo.rogpe@gmail.com>
2024-01-07 18:22:01 -0300

    modules

---
+++ modules.py
@@ -0,0 +1,19 @@
+# import math
+
+# from math import sqrt, sin
+
+# from math import *
+
+import math as m
+
+import numpy as np
+
+# import operadoresAri
+
+# print(m.sqrt(144))
+
+
+if __name__ == '__main__':
+    L = np.array([1,2,3,4,5,6,7,8,9])
+    print(L)
+    
